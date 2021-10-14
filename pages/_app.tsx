e4b8e31843b6ea39164a2e004a6802cac931b9bd
Johannes Filter <hi@jfilter.de>
2021-10-14 12:42:49 +0200

    Upgrade `mantine` to v3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,30 @@
+import { GlobalStyles, MantineProvider, NormalizeCSS } from "@mantine/core";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import {
-  MantineProvider,
-  NormalizeCSS,
-  GlobalStyles,
-  useStylesCleanup,
-  SsrProvider,
-} from "@mantine/core";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
-  useStylesCleanup();
-
   return (
     <>
-      <SsrProvider>
-        <Head>
-          <title>Mantine next example</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
 
-        <MantineProvider
-          theme={{
-            /* Put your mantine theme override here */
-            colorScheme: "light",
-          }}
-        >
-          {/* NormalizeCSS and GlobalStyles are optional */}
-          <NormalizeCSS />
-          <GlobalStyles />
-          <Component {...pageProps} />
-        </MantineProvider>
-      </SsrProvider>
+      <MantineProvider
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <NormalizeCSS />
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 }
