be3e2d3fc0274b81f1eac160fd14136d3b359840
Johannes Filter <hi@jfilter.de>
2021-11-30 00:01:20 +0100

    Add proper favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,25 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
