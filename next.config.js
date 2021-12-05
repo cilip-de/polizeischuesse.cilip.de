5ccba7300a03626c27d78da6a605b4d57d172378
Johannes Filter <hi@jfilter.de>
2021-12-05 19:23:58 +0100

    Add client-side caching headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  poweredByHeader: false,
   reactStrictMode: true,
   typescript: {
     // !! WARN !!
@@ -12,5 +13,38 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=900, stale-while-revalidate=3600",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          // {
+          //   key: "Content-Security-Policy",
+          //   value: "default-src https: 'unsafe-inline'",
+          // },
+          {
+            key: "X-Frame-Options",
+            value: "deny",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
   },
 };
