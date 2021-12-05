fe1a13685619ef4ee3b483b81ae2c4d7c36f07e6
Johannes Filter <hi@jfilter.de>
2021-12-05 20:58:40 +0100

    Add twitter account to meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,6 +33,7 @@ export default function App(props: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <meta name="twitter:site" content="@cilip_de" />
       </Head>
 
       <MantineProvider
