819df703b7690633f364e33e420eea5dfd7a35b9
Johannes Filter <hi@jfilter.de>
2021-12-09 19:35:59 +0100

    Add robots.txt + sitemap

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,45 @@
+import { setupData, setupTaserData } from "../lib/data";
+
+const hostname = "https://polizeischuesse.cilip.de";
+
+async function createSitemap(data: any[]) {
+  const caseRoutes = data.map((x) => `/fall/${x["Fall"]}`);
+
+  const localRoutes = [
+    "",
+    "/methodik",
+    "/visualisierungen",
+    "/taser",
+    "/statistik",
+    "/kontakt",
+    "/fall-melden",
+  ];
+
+  const pages = localRoutes.concat(caseRoutes);
+
+  const urlSet = pages
+    .map((page) => {
+      // Build url portion of sitemap.xml
+      return `<url><loc>${hostname}${page}</loc></url>`;
+    })
+    .join("");
+
+  // Add urlSet to entire sitemap string
+  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlSet}</urlset>`;
+}
+
+const Sitemap = () => {};
+
+Sitemap.getInitialProps = async ({ res, req }) => {
+  const { data } = await setupData();
+  const taserData = await setupTaserData();
+
+  const sitemap = await createSitemap(data.concat(taserData));
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+  return res;
+};
+
+export default Sitemap;
