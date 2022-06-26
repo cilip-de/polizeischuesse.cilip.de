961b65f6f451acdf3edf5f656858c68d9159d0c2
cilipde <108218047+cilipde@users.noreply.github.com>
2022-06-26 12:30:31 +0200

    Update kontakt.tsx

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -7,7 +7,7 @@ const privacyHtml = `
 <h3 id="dsg-general-intro"></h3>
 <p>Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend
   kurz „Daten“) im Rahmen der Erbringung unserer Leistungen sowie innerhalb unseres Onlineangebotes und der mit ihm verbundenen
-  Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unser Social Media Profile auf (nachfolgend
+  Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unsere Social Media Profile auf (nachfolgend
   gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder
   „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO).
   <br>
