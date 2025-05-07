c440224e91c43c5184df215751c6da0ac596bede
Johannes Filter <hi@jfilter.de>
2025-05-07 12:17:36 +0200

    Upgrade sentry

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
-  // if (process.env.NEXT_RUNTIME === "nodejs") {
-  //   await import("./sentry.server.config");
-  // }
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
 }
 
 export const onRequestError = Sentry.captureRequestError;
