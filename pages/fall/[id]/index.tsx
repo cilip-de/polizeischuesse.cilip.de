09d1505d0c964cfdb64af1588dd1b24039ade082
Johannes Filter <hi@jfilter.de>
2025-05-08 14:46:01 +0200

    Add share button to details page

--- pages/fall/[id]/index.tsx
+++ pages/fall/[id]/index.tsx
@@ -1,16 +1,50 @@
-import { Button, Center, Code, Container, Space, Text } from "@mantine/core";
+import {
+  Button,
+  Center,
+  Code,
+  Container,
+  Group,
+  Space,
+  Text,
+} from "@mantine/core";
 import { useClipboard } from "@mantine/hooks";
+import { IconCopy, IconShare } from "@tabler/icons-react"; // Import icons
 import dayjs from "dayjs";
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import Case from "../../../components/Case";
 import { setupData, setupTaserData } from "../../../lib/data";
 
 const CaseDetail: NextPage = (props) => {
   const clipboard = useClipboard({ timeout: 99999999999999 });
+  const [shareSupported, setShareSupported] = useState(false);
+
+  useEffect(() => {
+    // Check if Web Share API is supported
+    setShareSupported(typeof navigator !== "undefined" && !!navigator.share);
+  }, []);
 
   const isTaser = props.taser;
+  const pageUrl = `https://polizeischuesse.cilip.de/fall/${props.id}`;
+
+  const handleShare = async () => {
+    const title = `${
+      isTaser ? "Tod nach Tasereinsatz" : "Tod durch Polizeischuss"
+    } am ${props.case.datePrint} in ${props.case.place}`;
+    const text = props.case["Szenarium"];
+
+    try {
+      await navigator.share({
+        title,
+        // text,
+        url: pageUrl,
+      });
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  };
 
   const smTitle = `${
     isTaser ? "Tod nach Tasereinsatz" : "Tod durch Polizeischuss"
@@ -43,28 +77,44 @@ const CaseDetail: NextPage = (props) => {
       <Container>
         <div style={{ marginTop: "5rem" }}>
           <Text align="center">
-            <Link href={isTaser ? "/taser" : "/"}>Zur Chronik</Link>
+            <Link href={isTaser ? "/taser" : "/"}>
+              Zurück zur Chronik mit allen Fällen
+            </Link>
           </Text>
           <div style={{ marginTop: "2rem" }}>
             <Case item={props.case} hideLink isTaser={props.taser} />
           </div>
           <Center>
-            <Code className="only-non-mobile">{`https://polizeischuesse.cilip.de/fall/${props.id}`}</Code>
+            <Code className="only-non-mobile">{pageUrl}</Code>
             <Space w={"xl"} />
-            <Button
-              size="sm"
-              color={"gray"}
-              onClick={() =>
-                clipboard.copy(
-                  `https://polizeischuesse.cilip.de/fall/${props.id}`
-                )
-              }
-            >
-              {clipboard.copied ? "Link kopiert" : "Link kopieren"}
-            </Button>
+            <Group>
+              {shareSupported ? (
+                <Button
+                  size="sm"
+                  color="blue"
+                  leftIcon={<IconShare size={16} />}
+                  onClick={handleShare}
+                >
+                  Teilen
+                </Button>
+              ) : null}
+              <Button
+                size="sm"
+                color={"gray"}
+                leftIcon={<IconCopy size={16} />}
+                onClick={() => clipboard.copy(pageUrl)}
+              >
+                {clipboard.copied ? "Link kopiert" : "Link kopieren"}
+              </Button>
+            </Group>
           </Center>
           <Space h="xl" />
         </div>
+        <Text align="center">
+          <Link href={isTaser ? "/taser" : "/"}>
+            Zurück zur Chronik mit allen Fällen
+          </Link>
+        </Text>
       </Container>
     </>
   );
